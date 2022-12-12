3bf603c52e6f39b373cf836f557c478e3cdc1edc
wasaw <merenkovalexander@gmail.com>
2022-12-12 16:56:11 +0300

    Инициализация репозитория

---
+++ LessonSeventeen/LessonSeventeen.h
@@ -0,0 +1,18 @@
+//
+//  LessonSeventeen.h
+//  LessonSeventeen
+//
+//  Created by Александр Меренков on 12.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LessonSeventeen.
+FOUNDATION_EXPORT double LessonSeventeenVersionNumber;
+
+//! Project version string for LessonSeventeen.
+FOUNDATION_EXPORT const unsigned char LessonSeventeenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LessonSeventeen/PublicHeader.h>
+
+
